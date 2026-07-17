510b73b5dd4301bfe56d43dc486b8915957fcf27
Hemanth-HN264 <hemanthhn2004@gmail.com>
2026-07-17 12:15:15 +0530

    Time: 0 ms (100%), Space: 44.2 MB (72.4%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] pre=new int[nums.length];
+        int sum=0;
+        for(int i=0;i<=nums.length-1;i++){
+            sum=sum+nums[i];
+            pre[i]=sum;
+        }
+        return pre;
+    }
+}
