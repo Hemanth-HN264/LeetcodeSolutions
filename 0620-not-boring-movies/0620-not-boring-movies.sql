da606f6898dd05d8b6082cca97e47b0683f72fd7
Hemanth-HN264 <hemanthhn2004@gmail.com>
2026-07-23 22:08:34 +0530

    Time: 304 ms (18.07%), Space: 0B (100%) - LeetHub - Time: O(N log N), Space: O(N)

---
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT * FROM CINEMA
+WHERE ID%2!=0 AND DESCRIPTION!='BORING'
+ORDER BY RATING DESC;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
