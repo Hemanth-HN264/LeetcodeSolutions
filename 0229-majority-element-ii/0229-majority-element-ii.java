348d91c54e1f35b7e22f54333f0ed1a9369e8ad8
Hemanth-HN264 <hemanthhn2004@gmail.com>
2026-08-03 19:00:47 +0530

    Time: 12 ms (46.85%), Space: 51.3 MB (90.6%) - LeetHub - Time: O(n), Space: O(n

---
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int n=nums.length;
+        HashMap<Integer,Integer> h= new HashMap<>();
+        for(Integer i:nums){
+            int x=h.getOrDefault(i,0);
+            x++;
+            h.put(i,x);
+        }
+        int majority=n/3;
+        ArrayList<Integer> al=new ArrayList<>();
+        for(Integer i:h.keySet()){
+            if(h.get(i)>majority){
+                al.add(i);
+            }
+        }
+        return al;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
