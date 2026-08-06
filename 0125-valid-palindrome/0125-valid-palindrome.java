19e384024a9d84dab3b627938df7e2eade699c61
Hemanth-HN264 <hemanthhn2004@gmail.com>
2026-08-06 14:41:08 +0530

    Time: 3 ms (58.3%), Space: 45.2 MB (36.77%) - LeetHub - Time: O(n), Space: O(n)

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,26 +2,27 @@ class Solution {
     public boolean isPalindrome(String s) {
         char[] a=s.toCharArray();
         int i;
-        String ans="";
+        StringBuilder ans=new StringBuilder("");
         for(i=0;i<=a.length-1;i++){
             if(a[i]>=65 && a[i]<=90){
                 a[i]=(char)(a[i]+' ');
-                ans=ans+a[i];
+                ans.append(a[i]);
             }
             else if(a[i]>=97 && a[i]<=122){
-                ans=ans+a[i];
+                ans.append(a[i]);
             }
             else if(a[i]>=48 && a[i]<=57){
-                ans=ans+a[i];
+                ans.append(a[i]);
             }
             else if(a[i]==32){ 
             }
             else{  
+
             }
         }
 
         boolean flag= true;
-        char[] c=ans.toCharArray();
+        char[] c = ans.toString().toCharArray();
         i=0;
         int j=c.length-1;
         while(i<j){
