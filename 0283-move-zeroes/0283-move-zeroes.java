b666b786ca47b0167d8a6318e6c6394b2f8cc79a
Hemanth-HN264 <hemanthhn2004@gmail.com>
2026-09-06 23:47:08 +0530

    Time: 4 ms (9.2%), Space: 47.9 MB (38.16%) - LeetHub - Time: O(n), Space: O(1)

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,23 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i;
-        int left=0;
-        for(i=0;i<=nums.length-1;i++){
-                if(nums[i]!=0){
-                int temp=nums[i];
-                nums[i]=nums[left];
-                nums[left]=temp;
-                left++;
+        int j=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+
+        if(j == -1) {
+            return;
+        }
+        
+        for(int i=j+1; i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[i]=nums[i]^nums[j];
+                nums[j]=nums[i]^nums[j];
+                nums[i]=nums[i]^nums[j];
+                j++;
             }
         }
     }
