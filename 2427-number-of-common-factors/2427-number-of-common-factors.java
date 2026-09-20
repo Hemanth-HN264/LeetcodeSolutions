b08db00048d63f533d674328b17d1cd68289a87a
Hemanth-HN264 <hemanthhn2004@gmail.com>
2026-09-20 09:48:04 +0530

    Time: 1 ms (68.79%), Space: 42.2 MB (40.36%) - LeetHub - Time: O(min(a, b)), Space: O(1)

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int count=0;
+        int small=a;
+        if(a>b){
+            small=b;
+        }
+        for(int i=1;i<=small;i++){
+            if(a%i==0 && b%i==0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
