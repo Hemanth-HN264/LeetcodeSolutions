31c8b8e87bf0cd290d5f36132519b1fbe9c6626b
Hemanth-HN264 <hemanthhn2004@gmail.com>
2026-09-21 15:33:59 +0530

    Time: 0 ms (100%), Space: 48.2 MB (69.2%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
