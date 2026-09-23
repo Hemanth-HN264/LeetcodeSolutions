545d5bc291a5eaf940842c5d9c114d53c22cd313
Hemanth-HN264 <hemanthhn2004@gmail.com>
2026-09-23 16:01:11 +0530

    Time: 0 ms (100%), Space: 42.6 MB (39.5%) - LeetHub - Time: O(1), Space: O(1)

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,31 +1,6 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int sum1=0;
-        int sum2=0;
-        int series=1;
-        n=n*2;
-        while(n>0){
-            if(series%2!=0){
-                sum1=sum1+series;
-                series++;
-                n--;
-            }else{
-                sum2=sum2+series;
-                series++;
-                n--;
-            }
-        }
-        int small=sum1;
-        if(sum1>sum2){
-            small=sum2;
-        }
-        int gcd=0;
-        for(int i=1;i<=small;i++){
-            if(sum1%i==0 && sum2%i==0){
-                gcd=i;
-            }
-        }
-        return gcd;
+        return n;
     }
 }
 
