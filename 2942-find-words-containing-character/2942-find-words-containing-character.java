b864164f960b15901739896d3f34d93acc47af6c
Hemanth-HN264 <hemanthhn2004@gmail.com>
2026-09-26 18:48:03 +0530

    Time: 1 ms (100%), Space: 46.7 MB (35.25%) - LeetHub - Time: O(n * m), Space: O(k)

---
+++ 2942-find-words-containing-character/2942-find-words-containing-character.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
