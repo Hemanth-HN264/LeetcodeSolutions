daf02c6ff5b50902e228a4b7c200c98654bbedef
Hemanth-HN264 <hemanthhn2004@gmail.com>
2026-09-26 19:07:59 +0530

    Time: 1 ms (99.37%), Space: 46.6 MB (90.88%) - LeetHub - Time: O(n), Space: O(1)

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.java
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.java
@@ -1,28 +1,22 @@
 class Solution {
     public int[] transformArray(int[] nums) {
-        int j=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%2!=0){
-                j=i;
-                break;
-            }
-        }
-        for(int i=j+1;i<nums.length;i++){
-            if(nums[i]%2==0){
-                nums[i]=nums[i]^nums[j];
-                nums[j]=nums[i]^nums[j];
-                nums[i]=nums[i]^nums[j];
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
                 j++;
             }
         }
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==0){
-                nums[i]=0;
-            }
-            else{
-                nums[i]=1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                nums[i] = 0;
+            } else {
+                nums[i] = 1;
             }
         }
+
         return nums;
     }
 }
